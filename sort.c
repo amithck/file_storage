29f95adc71f5f5ecb0191c370efa45d0378dd4d9
amithck <87654235+amithck@users.noreply.github.com>
2022-02-23 23:57:20 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -2,7 +2,6 @@ void extension()
 {
   char ext[15];
   int loc;
-  int sizeq=sizeof(sort_queue)/sizeof(sort_queue[0]);
   for(int i=0;i<sizeq;i++)
   {
     loc=-1;
@@ -26,24 +25,63 @@ void sort(char ext[],int num)
 {
   char move_command[100]="mv ";
   strcat(move_command,sort_queue[num]);
+  
   int size_music=sizeof(music)/sizeof(music[0]);
   int size_exec=sizeof(exec)/sizeof(exec[0]);
+  int size_doc=sizeof(doc)/sizeof(doc[0]);
+  int size_img=sizeof(img)/sizeof(img[0]);
+  int size_vid=sizeof(doc)/sizeof(vid[0]);
+  
   for(int i=0;i<size_music;i++)
   {
     if(!(strcmp(ext,music[i])))
     {
-      strcat(move_command," music");
+      strcat(move_command," Music");
       system(move_command);
       return;
     }
   }
+  
   for(int i=0;i<size_exec;i++)
   {
     if(!(strcmp(ext,exec[i])))
     {
-      strcat(move_command," exec");
+      strcat(move_command," Executable");
       system(move_command);
       return;
     }
   }
+  
+  for(int i=0;i<size_vid;i++)
+  {
+    if(!(strcmp(ext,vid[i])))
+    {
+      strcat(move_command," Videos");
+      system(move_command);
+      return;
+    }
+  }
+  
+  for(int i=0;i<size_img;i++)
+  {
+    if(!(strcmp(ext,img[i])))
+    {
+      strcat(move_command," Images");
+      system(move_command);
+      return;
+    }
+  }
+  
+  for(int i=0;i<size_doc;i++)
+  {
+    if(!(strcmp(ext,doc[i])))
+    {
+      strcat(move_command," Documents");
+      system(move_command);
+      return;
+    }
+  }
+  
+  strcat(move_command," Unclassified");
+  system(move_command);
 }
